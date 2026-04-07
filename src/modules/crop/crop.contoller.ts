bc40a81106b51620d9118648c8c04c8bfc1b9ade
umayerCoding1 <umayerpro175@gmail.com>
2026-04-07 15:18:09 +0600

    feat: add crop recommendation controller using Gemini AI integration

--- src/modules/crop/crop.contoller.ts
+++ src/modules/crop/crop.contoller.ts
@@ -1,6 +1,7 @@
 import { asyncHandler } from "../../utils/asyncHandler";
 import { Request, Response } from "express";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import axios from "axios";
 
 const genAI = new GoogleGenerativeAI(process.env.GAN_AI_API_KEY!);
 
@@ -17,10 +18,10 @@ Soil: ${soil}
 
 response like this: 
 1:Crop name Bangla: (Short, simple Bangla te 4-5 ta crop suggestion dao.)
-1:Crop name English: base on crop name bangla and i was search in this english name in google and find this crop real image.
 2:Explination :(keno ai crop ta ai doroner matitir jono valo),
 3:Expected propit: {bace on Bangladesh marker value}
 4:rating: (1-10) {for best crop }
+5:image: (image url from  unsplash or pexels or any whear)
 this response send a array of object an json format
 
 Bullet point use koro.
@@ -30,11 +31,57 @@ Beshi kotha na.
     const result = await model.generateContent(prompt);
     const response = result.response;
     const text = response.text();
+    // if (text) {
+    //     const cleanString = text
+    //         .replace(/```json/g, '')
+    //         .replace(/```/g, '')
+    //         .trim()
+
+    //     const dataArray = JSON.parse(cleanString);
 
 
+
+    //     const cropsWithImages = await Promise.all(
+    //         dataArray.map(async (crop: any) => {
+    //             try {
+
+    //                 const pexelsRes = await axios.get(
+    //                     "https://api.pexels.com/v1/search",
+    //                     {
+    //                         params: {
+    //                             query: crop['Crop name Bangla'],
+    //                             per_page: 1
+    //                         },
+    //                         headers: {
+    //                             Authorization: process.env.PEXELS_API_KEY
+    //                         }
+    //                     }
+    //                 );
+    //                 console.log(pexelsRes)
+    //                 const imageUrl = pexelsRes.data.photos[0]?.src?.original
+    //                 return { ...crop, image: imageUrl }
+    //             } catch (error) {
+    //                 console.error('Error fetching image for', crop['Crop name'], error)
+    //                 return { ...crop, image: '' }
+    //             }
+    //         })
+    //     )
+
+    //     const maxRating = Math.max(...cropsWithImages.map((c: any) => c.Rating))
+    //     const bestCrop = cropsWithImages.find((c: any) => c.Rating === maxRating)
+
+    //     res.status(200).json({
+    //         success: true,
+    //         data: {
+    //             cropsWithImages,
+    //             bestCrop
+    //         }
+    //     });
+    // }
     res.status(200).json({
         success: true,
         data: text
     });
+
 });
 
